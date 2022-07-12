bbd22cd21c40a41973c2028911d5bef5d6ff117e
KhangTT <Khangtt@rikkeisoft.com>
2022-07-12 11:41:48 +0700

    Commit Initial Setup

---
+++ src/common/assets/index.js
@@ -0,0 +1,17 @@
+const images = {
+    logo: require('./VansLogoHeader.png'),
+    background: require('./VansBackground.jpg'),
+    card1: require('./VansCard01.jpg'),
+    card2: require('./VansCard02.jpg'),
+    card3: require('./VansCard03.jpg'),
+    card4: require('./VansCard04.jpg'),
+    card5: require('./VansCard05.jpg'),
+    clothes: require('./VansClothes.png'),
+    banner: require('./Picture04.png'),
+    picture1: require('./Picture01.jpg'),
+    picture2: require('./Picture02.jpg'),
+    picture3: require('./Picture03.jpg'),
+    banner2: require('./Picture05.png'),
+};
+
+export default images;
---
+++ src/config/RoutesPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Detail from '~/pages/Detail'
+import Home from '~/pages/Home'
+
+const RoutesPage = () => {
+  return (
+    <Routes>
+      <Route path='/product/:productId' element={<Detail />} />
+      <Route index element={<Home />} />
+    </Routes>
+  )
+}
+
+export default RoutesPage
